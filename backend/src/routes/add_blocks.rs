2eb933cd16602517f4653f33dad948e2c19873af
tr1xdev <55844996+tr1xdev@users.noreply.github.com>
2026-06-30 18:32:10 +0300

    feat(api): add validation and JSON error handling for block submission

--- backend/src/routes/add_blocks.rs
+++ backend/src/routes/add_blocks.rs
@@ -1,5 +1,6 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::Deserialize;
+use serde_json::json;
 
 use crate::AppState;
 
@@ -12,9 +13,28 @@ pub async fn post_block(
     State(state): State<AppState>,
     Json(payload): Json<AddBlockRequest>,
 ) -> impl IntoResponse {
+    // ensure that it's not an empty str
+    if payload.data.trim().is_empty() {
+        return (
+            StatusCode::BAD_REQUEST,
+            Json(json!({ "error": "Data cannot be empty" })),
+        )
+            .into_response();
+    }
+
     let mut chain = state.lock().unwrap();
+
     match chain.add_block(payload.data) {
-        Ok(()) => (StatusCode::CREATED, "Block added"),
-        Err(e) => (StatusCode::BAD_REQUEST, e),
+        Ok(()) => (
+            StatusCode::CREATED,
+            Json(json!({ "message": "Block added" })),
+        )
+            .into_response(),
+
+        Err(e) => (
+            StatusCode::BAD_REQUEST,
+            Json(json!({ "error": e.to_string() })),
+        )
+            .into_response(),
     }
 }
